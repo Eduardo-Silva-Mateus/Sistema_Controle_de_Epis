debbd30151a8753d3f2e2d5f90dfdc18dcd314ef
eduardoSilva <eduardomateussilva48@gmail.com>
2026-07-31 18:28:48 -0300

    Projeto antes da migração para VS code

---
+++ Controle_de_Epis/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Controle_de_Epis.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Controle_de_Epis.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser>  _signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login() 
+        {
+             return View();
+        }
+
+    }
+}
---
+++ Controle_de_Epis/Infrastructure/Identity/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Controle_de_Epis.Infrastructure.Identity
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Operador = "Operador";
+    }
+}
